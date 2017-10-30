91c6fc031e23333a0bcb882d5a702572755b57f5
Amber Burroughs <amberlburroughs@gmail.com>
2017-10-30 14:57:04 -0700

    add jawsDb

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var exphbs = require("method-override");
+var exphbs = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 3033;
+
+app.listen(port);
