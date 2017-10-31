1f4e4d27856fa7bbfccc3199f3b16fa2525cdc7e
Amber Burroughs <amberlburroughs@gmail.com>
2017-10-31 00:29:45 -0700

    add index view

--- server.js
+++ server.js
@@ -6,4 +6,17 @@ var app = express();
 
 var PORT = process.env.PORT || 3033;
 
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
 app.listen(port);
